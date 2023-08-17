bea6317b07847219f56f6942b71e987f66332550
t1ger-scripts <67964693+t1ger-scripts@users.noreply.github.com>
2023-08-17 06:03:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -44,6 +44,7 @@ BackEngineVehicles = {
 	Config.Price = 750
 
 cfg = {
+	degradeRandomComponents = true, 			-- Enable or disable degradation of random components (radiator, brakes etc). Set to false if you aren't used qb-mechanicjob.
 	deformationMultiplier = -1,					-- How much should the vehicle visually deform from a collision. Range 0.0 to 10.0 Where 0.0 is no deformation and 10.0 is 10x deformation. -1 = Don't touch. Visual damage does not sync well to other players.
 	deformationExponent = 0.5,					-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 	collisionDamageExponent = 0.5,				-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
